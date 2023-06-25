e4f9556e77d6d4369b999cb38239eb37bcf07e76
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-06-25 14:06:42 +0500

    TEMPLATE COMPLETED

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+import Bard, { askAI } from "bard-ai";
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+dotenv.config();
+
+// Retrieve the API key from the environment variables file
+const apiKey = 'XQimvVD1RUeYIhVVJKDM-oilymZHrWdzXIuwZjiTdcxuwYFZ1qNgE7MDBvgMwrEykrp0XQ.'
+if (!apiKey) {
+  console.error("BARD_API_KEY not found in environment variables");
+  process.exit(1);
+}
+
+(async () => {
+  try {
+    await Bard.init(apiKey);
+    console.log("Bard initialized successfully");
+  } catch (error) {
+    console.error("Error initializing Bard:", error);
+    process.exit(1);
+  }
+})();
+
+const prompt2 =
+  "You are a professional Chatbot integrated into ONE Technology Services' website, a software company offering a wide range of software services. Your role is to provide concise and informative information about the company's services. If users wish to contact the company, they can do so through LinkedIn (https://www.linkedin.com/company/one-technology-services/), Twitter (https://twitter.com/ONETechnologySer) and can email us on our email (info@onetechnologyservices.com). Please provide a response to the following question regarding ONE Technology Services' software services.";
+
+app.use(cors());
+
+app.get("/create-response/:prompt", async (req, res) => {
+  const prompt = req.params.prompt;
+  try {
+    const response = await Bard.askAI(prompt2 + prompt);
+    console.log(response);
+    res.json({ message: response });
+  } catch (error) {
+    console.error("Error generating response:", error);
+    res.status(500).json({ message: "Error generating response" });
+  }
+});
+
+app.listen(5000 || process.env.PORT , () => {
+  console.log("Server started");
+});
